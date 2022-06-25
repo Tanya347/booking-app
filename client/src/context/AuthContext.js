71d601cf72990be1213aaf3855253d1c24f4e8d4
Panda <tanyadiit7@gmail.com>
2022-06-25 13:39:52 +0530

    finished search context

---
+++ client/src/context/AuthContext.js
@@ -0,0 +1,29 @@
+import { createContext, useReducer } from "react"
+
+const INITIAL_STATE = {
+
+}
+
+export const AuthContext = createContext(INITIAL_STATE)
+
+const AuthReducer = (state, action) => {
+    switch (action.type) {
+        case "NEW_AUTH":
+            return action.payload
+        case "RESET_AUTH":
+            return INITIAL_STATE
+        default:
+            return state;
+    }
+}
+
+export const AuthContextProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE)
+    return (
+        <AuthContext.Provider
+            value={{ city: state.city, dates: state.dates, options: state.options, dispatch }}
+        >
+            {children}
+        </AuthContext.Provider>
+    )
+}
--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -12,7 +12,7 @@ const List = () => {
 
     const location = useLocation();
     const [destination, setDestination] = useState(location.state.destination);
-    const [date, setDate] = useState(location.state.date);
+    const [dates, setDates] = useState(location.state.dates);
     const [openDate, setOpenDate] = useState(false);
     const [options, setOptions] = useState(location.state.options);
     const [min, setMin] = useState(undefined);
@@ -35,8 +35,8 @@ const List = () => {
                         </div>
                         <div className="lsItem">
                             <label className="white">Check-in Date</label>
-                            <span onClick={() => setOpenDate(!openDate)}>{`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(date[0].endDate, "MM/dd/yyyy")}`}</span>
-                            {openDate && <DateRange onChange={(item) => setDate([item.selection])} minDate={new Date()} ranges={date} />}
+                            <span onClick={() => setOpenDate(!openDate)}>{`${format(dates[0].startDate, "MM/dd/yyyy")} to ${format(dates[0].endDate, "MM/dd/yyyy")}`}</span>
+                            {openDate && <DateRange onChange={(item) => setDates([item.selection])} minDate={new Date()} ranges={dates} />}
                         </div>
                         <div className="lsItem">
                             <label className="white">Options</label>
