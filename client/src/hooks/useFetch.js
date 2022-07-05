28058b3de2b1f3fd94803d549664c7d17c5a130a
Panda <tanyadiit7@gmail.com>
2022-07-05 11:59:09 +0530

    last last

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -13,7 +13,7 @@ const useFetch = (url) => {
         const fetchData = async () => {
             setLoading(true);
             try {
-                const res = await axios.get(`${API_URL}/${url}`);
+                const res = await axios.get(`${API_URL}${url}`);
                 setData(res.data);
             } catch (err) {
                 setError(err);
